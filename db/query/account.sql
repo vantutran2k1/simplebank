b6d6d76ab2f4564b0527635bb2c9829312c931e7
vantutran2k1 <vantu.tran2k1@gmail.com>
2024-02-28 23:17:35 +0700

    Create unit test for CRUD operations of Account model

--- db/query/account.sql
+++ db/query/account.sql
@@ -16,10 +16,8 @@ LIMIT 1;
 -- name: ListAccounts :many
 SELECT *
 FROM accounts
-WHERE owner = $1
-ORDER BY id
-LIMIT $2
-OFFSET $3;
+LIMIT $1
+OFFSET $2;
 
 -- name: UpdateAccount :one
 UPDATE accounts
