c9e2e9a6bd1e88d72344907a8f174cf1565f1141
vantutran2k1 <vantu.tran2k1@gmail.com>
2024-03-02 14:11:20 +0700

    Read environment variable with viper

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"database/sql"
+	"github.com/vantutran2k1/simplebank/util"
 	"log"
 	"os"
 	"testing"
@@ -12,15 +13,13 @@ import (
 var testQueries *Queries
 var testDB *sql.DB
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-)
-
 func TestMain(m *testing.M) {
-	var err error
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
 
-	testDB, err = sql.Open(dbDriver, dbSource)
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
 	}
